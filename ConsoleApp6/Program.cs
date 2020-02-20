f12c099a3c95b46a7b96b62fdf0496c54944b807
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:45:57 +0530

    Initial commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+
+        public static double avg(int[] marks)
+        {
+
+            double a;
+            int i;
+            double s = 0;
+            int length = marks.Length;
+
+
+            for (int i = 0; i < Length; i++)
+            {
+                s = s + marks[i];
+            }
+            a = s / length;
+            return a;
+        }
+           static void Main(string[] args)
+        {
+            int i;
+            int[] marks = new int[] { 80, 90, 87, 65 };
+            string[] names = new string[] { "chittu", "chotu", "motu" };
+        }
+        }
+    }
+}
+
+
+
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
